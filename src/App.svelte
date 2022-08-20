6ee92ea6828e25018f964f3b7d87785b13dcb9ab
hansburnthem <hnifrma@gmail.com>
2022-08-20 07:52:00 +0700

    feat: create navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script>
-  
+    import Navbar from "./component/Navbar.svelte";
 </script>
 
 <main>
+    <Navbar/>
+    <!-- <div class="flex flex-wrap w-[1200px]">
+        {#each movies as movie}
+            <div class="p-2 flex flex-col w-[240px]">
+                <img src={'https://image.tmdb.org/t/p/original' + movie.poster_path} alt={movie.original_title}>
+                <h1 class="font-bold text-3xl text-center py-2">{movie.original_title}</h1>
+                <p class="text-justify">{movie.overview}</p>
+            </div>
+        {/each}
+    </div> -->
   
 </main>
---
+++ src/component/Navbar.svelte
@@ -0,0 +1,10 @@
+<script>
+    
+</script>
+
+<div class="w-screen p-5 flex flex-row justify-center font-mono">
+    <a href="#">1</a>
+    <a href="#">1</a>
+    <a href="#">1</a>
+    <a href="#">1</a>
+</div>
